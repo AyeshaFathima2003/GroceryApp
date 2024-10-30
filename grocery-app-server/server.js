104f08f8cd4aa14efcd4d97772bf7b9f8e16c86a
Azsaria114 <susiazsaria@gmail.com>
2024-10-30 23:06:48 +0530

    userProfile

--- grocery-app-server/server.js
+++ grocery-app-server/server.js
@@ -1,8 +1,10 @@
 // server.js
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const userRoutes = require('./routes/userRoutes');
 const dotenv = require('dotenv');
+
 
 // Connect to MongoDB
 const connectDB = require('./config/db');
@@ -15,6 +17,8 @@ const app = express();
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // Routes
