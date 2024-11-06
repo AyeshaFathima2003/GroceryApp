27e7e500f81d7c3e9878e66139ebb61d9d986d22
Azsaria114 <susiazsaria@gmail.com>
2024-11-06 22:11:50 +0530

    wishlist_cart

--- grocery-app-server/routes/userRoutes.js
+++ grocery-app-server/routes/userRoutes.js
@@ -2,9 +2,10 @@
 const express = require('express');
 
 const router = express.Router();
-const { signup,login,getUserProfile,logoutUser,updateUserProfile,addUserAddress,getUserAddresses,getUserAddresses} = require('../controller/userController');
+const { signup,login,getUserProfile,logoutUser,updateUserProfile,addUserAddress,getUserAddresses,addToWishlist,removeFromWishlist,getWishlist,addToCart,updateCart,getCart,placeOrder,getUserOrders} = require('../controller/userController');
 
 const{verifyToken}=require('../utils/verifyToken');
+
 
 // Signup route
 router.post('/signup', signup);
@@ -16,5 +17,12 @@ router.put('/update',updateUserProfile);
 router.post('/address', addUserAddress);
 router.get('/addresses', getUserAddresses);
 router.post('/wishlist', addToWishlist);
+router.delete('/wishlist', removeFromWishlist);
+router.get('/wishlist',getWishlist);
+router.post('/cartadd', addToCart);
+router.put('/updatecart',updateCart);
+router.get('/getcart', getCart);
+router.post('/placeOrder', placeOrder);
+router.get('/userOrders', getUserOrders);
 
 module.exports = router;
