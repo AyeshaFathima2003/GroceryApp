f63b33d0ff315d297c58ad80f680ccd5703f6db6
GIBSONGODSAN <gibson.25cs@licet.ac.in>
2024-11-07 19:18:08 +0530

    project structured properly

--- grocery-app-interface/src/app/app.routes.ts
+++ grocery-app-interface/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
 
 export const routes: Routes = [
-    {path: 'login', component: LoginComponent},
-    {path: 'signup', component: SignupComponent}
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/auth/login' }  // Wildcard route for a 404 page (optional)
 ];
 
--- grocery-app-interface/src/app/auth/login/login.component.ts
+++ grocery-app-interface/src/app/auth/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthService } from '../../service/auth.service';
+import { AuthService } from '../../core/services/auth.service';
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
